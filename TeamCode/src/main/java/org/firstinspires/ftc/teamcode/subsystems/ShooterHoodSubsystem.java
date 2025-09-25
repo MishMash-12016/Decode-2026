c72dff6da2e2cb560c80923576d3ec343f7b7287
itay12016 <itayperetz87@gmail.com>
2025-09-25 17:59:38 +0300

    itay add exterpolationMap for the shuter

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ShooterHoodSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ShooterHoodSubsystem.java
@@ -1,10 +1,14 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
 import com.acmerobotics.dashboard.config.Config;
+import com.seattlesolvers.solverslib.command.Command;
 
 import org.firstinspires.ftc.teamcode.Libraries.CuttlefishFTCBridge.src.utils.Direction;
 import org.firstinspires.ftc.teamcode.Libraries.MMLib.Subsystems.Servo.ServoSubsystem;
+import org.firstinspires.ftc.teamcode.Libraries.MMLib.Utils.exterpolation.ExterpolationMap;
 import org.firstinspires.ftc.teamcode.MMRobot;
+
+import java.util.function.DoubleSupplier;
 
 import Ori.Coval.Logging.AutoLog;
 
@@ -12,6 +16,9 @@ import Ori.Coval.Logging.AutoLog;
 @AutoLog
 
 public class ShooterHoodSubsystem extends ServoSubsystem {
+
+    ExterpolationMap exterpolationMap = new ExterpolationMap()
+            .put(5,5);
 
     //TODO: generic values
     public static double hoodUp = 0.09;
@@ -34,5 +41,10 @@ public class ShooterHoodSubsystem extends ServoSubsystem {
         MMRobot mmRobot = MMRobot.getInstance();
 
         withServo("shooterHoodServo",Direction.FORWARD,0);
+    }
+
+    public Command aimHoodToShut(DoubleSupplier distance) {
+        return setPositionCommand(
+                ()-> exterpolationMap.exterpolate(distance.getAsDouble()));
     }
 }
