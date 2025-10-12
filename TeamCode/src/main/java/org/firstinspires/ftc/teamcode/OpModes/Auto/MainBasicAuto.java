ae2488108159485027b71c4bd1b2e2e8920c5ae4
itay12016 <itayperetz87@gmail.com>
2025-10-12 15:55:39 +0300

    base main auto (not tested)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Auto/MainBasicAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Auto/MainBasicAuto.java
@@ -1,4 +1,61 @@
 package org.firstinspires.ftc.teamcode.OpModes.Auto;
 
-public class MainBasicAuto {
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.PathChain;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.seattlesolvers.solverslib.command.SequentialCommandGroup;
+
+import org.firstinspires.ftc.teamcode.CommandGroups.ShootCommandGroup;
+import org.firstinspires.ftc.teamcode.Libraries.MMLib.MMDrivetrain;
+import org.firstinspires.ftc.teamcode.Libraries.MMLib.MMOpMode;
+import org.firstinspires.ftc.teamcode.Libraries.MMLib.Utils.OpModeVeriables.OpModeType;
+import org.firstinspires.ftc.teamcode.Libraries.pedroPathing.FollowPathCommand;
+import org.firstinspires.ftc.teamcode.subsystems.ShooterSubsystem;
+
+@Autonomous
+public class MainBasicAuto extends MMOpMode {
+
+    private PathChain FROM_START_TO_SCORE, FROM_SCORE_TO_PARKING;
+
+    private final Pose startPose = new Pose(104.000, 136.000,Math.toRadians(-90));
+    private final Pose scorePose = new Pose(105.000, 105.000,Math.toRadians(50));
+    private final Pose parkingPose = new Pose(90.000, 55.000,Math.toRadians(90));
+
+    public void buildPaths() {
+        FROM_START_TO_SCORE = MMDrivetrain.follower.pathBuilder()
+                .addPath(new BezierLine(startPose,scorePose))
+                .setLinearHeadingInterpolation(Math.toRadians(90), Math.toRadians(135))
+                .build();
+        FROM_SCORE_TO_PARKING = MMDrivetrain.follower.pathBuilder()
+                .addPath(new BezierLine(scorePose,parkingPose))
+                .setTangentHeadingInterpolation().build();
+    }
+
+
+    public MainBasicAuto() {
+        super(OpModeType.NonCompetition.DEBUG);
+    }
+
+    @Override
+    public void onInit() {
+        MMDrivetrain.getInstance().update();
+        MMDrivetrain.follower.setPose(startPose);
+        buildPaths();
+        Follower follower = MMDrivetrain.follower;
+
+        SequentialCommandGroup autonomousSequence = new SequentialCommandGroup(
+                new FollowPathCommand(follower, FROM_START_TO_SCORE),
+                ShootCommandGroup.ShootAll(),
+                new FollowPathCommand(follower, FROM_SCORE_TO_PARKING)
+        );
+        autonomousSequence.schedule();
+    }
+
+    @Override
+    public void onPlayLoop() {
+        MMDrivetrain.getInstance().update();
+    }
+
 }
